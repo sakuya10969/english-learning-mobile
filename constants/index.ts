5d3c79256cb3d5670ab440d0342c4b1a713c8650
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-01 11:44:03 +0900

    feat: constants

---
+++ constants/index.ts
@@ -0,0 +1,63 @@
+export const grammarSections = [
+    {
+      title: '時制',
+      description: '現在形・過去形・未来形・完了形など',
+      link: '/grammar/tense',
+    },
+    {
+      title: '受動態',
+      description: '受け身の表現: be動詞 + 過去分詞',
+      link: '/grammar/passive',
+    },
+    {
+      title: '助動詞',
+      description: 'can, must, should など可能性・義務の表現',
+      link: '/grammar/modal',
+    },
+    {
+      title: '仮定法',
+      description: 'If を使った仮定の表現（仮定法過去・過去完了など）',
+      link: '/grammar/conditional',
+    },
+    {
+      title: '関係詞',
+      description: 'who, which, that などによる節の追加説明',
+      link: '/grammar/relative',
+    },
+    {
+      title: '分詞',
+      description: '現在分詞・過去分詞による修飾',
+      link: '/grammar/participle',
+    },
+    {
+      title: '比較',
+      description: '形容詞の比較級・最上級の使い方',
+      link: '/grammar/comparative',
+    },
+    {
+      title: '不定詞・動名詞',
+      description: 'to + 動詞、動詞 + ing の違いと使い分け',
+      link: '/grammar/infinitive-gerund',
+    },
+    {
+      title: '前置詞',
+      description: 'at, in, on など場所・時間の前置詞',
+      link: '/grammar/preposition',
+    },
+    {
+      title: '接続詞',
+      description: 'and, but, because, although などの使い方',
+      link: '/grammar/conjunction',
+    },
+    {
+      title: '冠詞',
+      description: 'a, an, the の使い分け',
+      link: '/grammar/article',
+    },
+    {
+      title: '疑問文・否定文',
+      description: 'do/does/did を使った疑問文と否定文の作り方',
+      link: '/grammar/question-negative',
+    },
+  ];
+  
