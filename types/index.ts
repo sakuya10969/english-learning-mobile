53ba0f6a7623ecf3d10f1c25d0e1898f99d3c804
sakuya10969 <sakuya.f20010618@gmail.com>
2025-07-01 12:12:51 +0900

    feat: types

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export interface HomeSectionCardProps {
+  title: string;
+  description: string;
+  link: string;
+  index?: number;
+};
+
+export interface GrammarSectionCardProps {
+  title: string;
+  description: string;
+  link: string;
+  index: number;
+}
